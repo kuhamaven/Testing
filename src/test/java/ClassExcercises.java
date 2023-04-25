33d42fe5c8b77d1c77e730cffbc0d1929f287bd2
Kuha <khalil.stessens@ing.austral.edu.ar>
2023-04-25 19:29:13 -0300

    consulta pre parcial

--- src/test/java/ClassExcercises.java
+++ src/test/java/ClassExcercises.java
@@ -60,4 +60,55 @@ public class ClassExcercises {
 
         return true;
     }
+
+    private void insertAfter(Node first, Node second){
+        if(first == null || second == null) return;
+
+        Node aux = first.next;
+        first.next = second;
+        second.next = aux;
+    }
+
+    private class Node<E>{
+        E item;
+        Node next;
+    }
+
+    @Test
+    public void addParenthesis(){
+        String input =  "1+2)*3-4)*5-6)))";
+        // ((1 + 2 ) * ((3 - 4) * (5 - 6 ) ) )
+
+        Stack<Character> stack = new Stack<>();
+
+        char[] chars = input.toCharArray();
+        for(char c: chars) {
+            stack.push(c);
+        }
+
+        String result = "";
+        int parenthesisCount = 0;
+
+        while(!stack.isEmpty()){
+            char c = stack.pop();
+
+            switch (c){
+                case ')':
+                    parenthesisCount += 1;
+                    result = c + result;
+                    break;
+                case '-':
+                case '+': result = c + result;
+                    c = stack.pop();
+                    result = "(" + c + result;
+                    parenthesisCount -= 1;
+                    break;
+                default: result = c + result;
+            }
+        }
+
+        //for (int i = 0; i<parenthesisCount; i++) result = "(" + result;
+
+        System.out.println(result);
+    }
 }
